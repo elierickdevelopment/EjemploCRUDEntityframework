e204c91b6d40cdbf0d7ddcfd87021c9cee1de9dd
ErickCastillo <elierick2013@gmail.com>
2022-02-09 10:21:26 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace EjemploCRUDEntityframework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_agregar = new System.Windows.Forms.Button();
+            this.button_GuardarEdicion = new System.Windows.Forms.Button();
+            this.button_Eliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button_agregar
+            // 
+            this.button_agregar.Location = new System.Drawing.Point(69, 13);
+            this.button_agregar.Name = "button_agregar";
+            this.button_agregar.Size = new System.Drawing.Size(75, 23);
+            this.button_agregar.TabIndex = 1;
+            this.button_agregar.Text = "Agregar";
+            this.button_agregar.UseVisualStyleBackColor = true;
+            this.button_agregar.Click += new System.EventHandler(this.button_agregar_Click);
+            // 
+            // button_GuardarEdicion
+            // 
+            this.button_GuardarEdicion.Location = new System.Drawing.Point(177, 12);
+            this.button_GuardarEdicion.Name = "button_GuardarEdicion";
+            this.button_GuardarEdicion.Size = new System.Drawing.Size(143, 23);
+            this.button_GuardarEdicion.TabIndex = 2;
+            this.button_GuardarEdicion.Text = "Guardar Edición";
+            this.button_GuardarEdicion.UseVisualStyleBackColor = true;
+            this.button_GuardarEdicion.Click += new System.EventHandler(this.button_GuardarEdicion_Click);
+            // 
+            // button_Eliminar
+            // 
+            this.button_Eliminar.Location = new System.Drawing.Point(350, 12);
+            this.button_Eliminar.Name = "button_Eliminar";
+            this.button_Eliminar.Size = new System.Drawing.Size(113, 23);
+            this.button_Eliminar.TabIndex = 3;
+            this.button_Eliminar.Text = "Eliminar Row";
+            this.button_Eliminar.UseVisualStyleBackColor = true;
+            this.button_Eliminar.Click += new System.EventHandler(this.button_Eliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_Eliminar);
+            this.Controls.Add(this.button_GuardarEdicion);
+            this.Controls.Add(this.button_agregar);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button_agregar;
+        private System.Windows.Forms.Button button_GuardarEdicion;
+        private System.Windows.Forms.Button button_Eliminar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EjemploCRUDEntityframework.Models;
+
+namespace EjemploCRUDEntityframework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            CargarGrid();
+         
+        }
+
+        private void CargarGrid() {
+
+            using (TestEntities dbContext = new TestEntities())
+            {
+                var list = from d in dbContext.TablaPersonas select d;
+                dataGridView1.DataSource = list.ToList();
+            }
+
+        }
+
+        private void AgregarNuevo()
+        {
+
+            using (TestEntities dbContext = new TestEntities())
+            {
+                TablaPersonas t = new TablaPersonas();
+                t.Nombre = "Nuevo Nombre desde EF " + DateTime.Now.ToString();
+                dbContext.TablaPersonas.Add(t);
+                dbContext.SaveChanges();
+                
+            }
+
+        }
+
+        private void GuardarModificacion() {
+
+            int? idRow = GetId();
+            using (TestEntities dbContext = new TestEntities())
+            {
+                if (idRow != null) {
+
+                    TablaPersonas t = new TablaPersonas();
+                    t.Nombre = "Dato Modificado..";
+                    dbContext.Entry(t).State = System.Data.Entity.EntityState.Added;
+                    
+                    dbContext.SaveChanges();
+                    CargarGrid();
+                }
+            }
+        }
+
+
+        private void EliminarRow()
+        {
+
+            int? id = GetId();
+            if (id != null)
+            {
+                using (TestEntities dbContext = new TestEntities())
+                {
+                    TablaPersonas t = dbContext.TablaPersonas.Find(id);
+                    dbContext.TablaPersonas.Remove(t);
+
+                    dbContext.SaveChanges();
+                }
+
+                CargarGrid();
+
+            }
+        }
+
+        private int? GetId()
+        {
+            try
+            {
+                return int.Parse(dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString());
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void button_agregar_Click(object sender, EventArgs e)
+        {
+            AgregarNuevo();
+            CargarGrid();
+        }
+
+        private void button_GuardarEdicion_Click(object sender, EventArgs e)
+        {
+            GuardarModificacion();
+        }
+
+        private void button_Eliminar_Click(object sender, EventArgs e)
+        {
+            EliminarRow();
+        }
+    }
+}
